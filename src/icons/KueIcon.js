40e7f89b1d6c59c211ade3d89ccd044b634322aa
Momo9414 <omomo9414@gmail.com>
2025-05-18 23:25:50 +0000

    Premier commit

---
+++ src/icons/KueIcon.js
@@ -0,0 +1,14 @@
+// src/icons/KueIcon.js
+import React from 'react';
+
+const KueIcon = () => (
+  <svg width="40" height="40" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="40" cy="40" r="40" fill="#D4E6FF"/>
+    <rect x="22" y="22" width="36" height="36" rx="4" fill="#2F80ED"/>
+    <path d="M28 32H52" stroke="white" stroke-width="3" stroke-linecap="round"/>
+    <path d="M28 40H48" stroke="white" stroke-width="3" stroke-linecap="round"/>
+    <path d="M28 48H44" stroke="white" stroke-width="3" stroke-linecap="round"/>
+  </svg>
+);
+
+export default KueIcon;
---
+++ src/icons/UserIcon.js
@@ -0,0 +1,12 @@
+// src/icons/UserIcon.js
+import React from 'react';
+
+const UserIcon = () => (
+  <svg width="40" height="40" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="40" cy="40" r="40" fill="#E3F2FD"/>
+    <path d="M40 40C45.5228 40 50 35.5228 50 30C50 24.4772 45.5228 20 40 20C34.4772 20 30 24.4772 30 30C30 35.5228 34.4772 40 40 40Z" fill="#2F80ED"/>
+    <path d="M60 60C60 49.05 51.05 40 40 40C28.95 40 20 49.05 20 60" stroke="#2F80ED" stroke-width="6" stroke-linecap="round"/>
+  </svg>
+);
+
+export default UserIcon;
